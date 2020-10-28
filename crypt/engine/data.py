f690a8b26784dd91a88cbc4b9b2839affe13b892
SteveImmanuel <steveandreasimanuel@gmail.com>
2020-10-28 23:27:59 +0700

    Edit data class

--- crypt/engine/data.py
+++ crypt/engine/data.py
@@ -8,6 +8,8 @@ class DataType(Enum):
 
 
 class Data():
-    def __init__(self, data_type: DataType, value: str):
-        self.type = data_type
+    def __init__(self, data_type: DataType, value: str, output_path: str = None):
+        self.data_type = data_type
         self.value = value
+        if self.data_type == DataType.FILE:
+            self.output_path = output_path
