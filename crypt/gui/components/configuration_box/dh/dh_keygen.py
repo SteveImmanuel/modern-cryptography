a22c018b6baacbcaef95274ba9cc4066c6554334
willysantoso05 <willysantoso05@gmail.com>
2020-10-29 14:36:05 +0700

    feat: Update DiffieHellman Keygen

--- crypt/gui/components/configuration_box/dh/dh_keygen.py
+++ crypt/gui/components/configuration_box/dh/dh_keygen.py
@@ -5,7 +5,8 @@ from PyQt5 import QtCore
 from crypt.gui.components.configuration_box.base_keygen import BaseKeygen
 from crypt.gui.components.configuration_box.edit_with_button import EditWithButton
 from crypt.engine.key import *
-from crypt.utils.number_util import generate_prime_number
+from crypt.gui.dialog_window import DialogWindow
+from crypt.utils.number_util import *
 
 
 class DHKeygen(BaseKeygen):
@@ -14,28 +15,64 @@ class DHKeygen(BaseKeygen):
         self.setup_ui()
 
     def setup_ui(self):
-        self.p_value = EditWithButton('p value', 'Randomize')
-        self.q_value = EditWithButton('q value', 'Randomize')
+        self.n_value = EditWithButton('n value', 'Randomize')
+        self.g_value = EditWithButton('g value', 'Randomize')
+        self.x_value = EditWithButton('x value', 'Randomize')
+        self.y_value = EditWithButton('y value', 'Randomize')
         self.spacer = QSpacerItem(10, 10, QSizePolicy.Expanding, QSizePolicy.Expanding)
 
         self.layout = QVBoxLayout()
-        self.layout.addWidget(self.p_value)
-        self.layout.addWidget(self.q_value)
+        self.layout.addWidget(self.n_value)
+        self.layout.addWidget(self.g_value)
+        self.layout.addWidget(self.x_value)
+        self.layout.addWidget(self.y_value)
         self.layout.addSpacerItem(self.spacer)
 
         self.setLayout(self.layout)
 
-        self.p_value.btn_random.clicked.connect(lambda: self.randomize(is_p=True))
-        self.q_value.btn_random.clicked.connect(lambda: self.randomize(is_p=False))
+        self.n_value.btn_random.clicked.connect(lambda: self.randomize_prime(is_n=True))
+        self.g_value.btn_random.clicked.connect(lambda: self.randomize_prime(is_n=False))
+        self.x_value.btn_random.clicked.connect(lambda: self.randomize_number(is_x=True))
+        self.y_value.btn_random.clicked.connect(lambda: self.randomize_number(is_x=False))
 
-    def randomize(self, is_p: bool):
-        random_number = generate_prime_number(9)
-        if is_p:
-            self.p_value.line_edit.setText(str(random_number))
+    def randomize_prime(self, is_n: bool):
+        if(is_n):
+            random_prime = generate_prime_number(10)
+            self.n_value.line_edit.setText(str(random_prime))
         else:
-            self.q_value.line_edit.setText(str(random_number))
+            try:
+                n = int(self.n_value.line_edit.text())
+
+                if is_prime(n):
+                    random_prime = generate_prime_number(9)
+                    self.g_value.line_edit.setText(str(random_prime))
+                    
+                else:
+                    DialogWindow('Invalid', 'n value must be a prime number').exec_()
+
+            except:
+                DialogWindow('Invalid', 'n value must be a prime number').exec_()
+
+
+    def randomize_number(self, is_x: bool):
+        try:
+            n = int(self.n_value.line_edit.text())
+
+            if is_prime(n):
+                random_number = generate_random_number(1, n - 1)
+                if is_x:
+                    self.x_value.line_edit.setText(str(random_number))
+                else:
+                    self.y_value.line_edit.setText(str(random_number))
+            else:
+                DialogWindow('Invalid', 'n value must be a prime number').exec_()
+
+        except:
+            DialogWindow('Invalid', 'p value must be a prime number').exec_()
 
     def build_params(self):
-        p = int(self.p_value.line_edit.text())
-        q = int(self.q_value.line_edit.text())
-        return [p, q]
+        n = int(self.n_value.line_edit.text())
+        g = int(self.g_value.line_edit.text())
+        x = int(self.x_value.line_edit.text())
+        y = int(self.y_value.line_edit.text())
+        return [n, g, x, y]
