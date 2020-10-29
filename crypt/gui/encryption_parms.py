32a5ae7dd3adc9c24c27940b79e5b835e29227b2
SteveImmanuel <steveandreasimanuel@gmail.com>
2020-10-29 12:02:01 +0700

    Remove deprecated class

--- crypt/gui/encryption_parms.py
+++ crypt/gui/encryption_parms.py
@@ -18,15 +18,6 @@ class ModeType(Enum):
     DECRYPT = 'Decrypt'
 
 
-class OutputType(Enum):
-    NO_SPACE = 'Remove whitespace'
-    GROUP = 'Group in 5'
-
-    @staticmethod
-    def list():
-        return list(map(lambda out_type: out_type, OutputType))
-
-
 class EncryptionParms:
     __instance = None
 
@@ -40,13 +31,11 @@ class EncryptionParms:
             self.engine = None
             self.engine_type = EngineType.RSA
             self.old_engine_type = None
-            self.output_conf = None
 
     def print_info(self):
         print('***Encryption Parameters***')
         print('Mode:', self.mode)
         print('Engine Type:', self.engine_type)
-        print('Output Configuration:', self.output_conf)
         print('***************************')
 
     def update_engine_type(self, engine_type: EngineType):
@@ -54,18 +43,10 @@ class EncryptionParms:
         self.engine_type = engine_type
         self.signal.engine_type.emit(engine_type)
 
-    def update_output_type(self, output_type: OutputType):
-        self.output_conf = output_type
-        self.signal.output_type.emit(output_type)
-
-    def is_parameter_valid(self) -> bool:
-        return True
-
     def get_engine(self) -> BaseEngine:
-        if self.engine_type == self.old_engine_type:
-            return self.engine
-        else:
-            return EngineFactory.create_engine(self.engine_type)
+        if self.engine_type != self.old_engine_type:
+            self.engine = EngineFactory.create_engine(self.engine_type)
+        return self.engine
 
     @staticmethod
     def get_instance() -> EncryptionParms:
